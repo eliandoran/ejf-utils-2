e44b64573bb41cc0fce3971ad053e5b7f4cca519
Elian Doran <contact@eliandoran.me>
2024-07-24 17:15:27 +0300

    Implement display of diff characters

--- diff.ts
+++ diff.ts
@@ -1,29 +1,42 @@
 const [ firstFilePath, secondFilePath ] = Deno.args;
+import { printImage } from "https://deno.land/x/terminal_images@3.1.0/mod.ts";
+import { BlobWriter } from "jsr:@zip-js/zip-js";
+import { Entry } from "jsr:@zip-js/zip-js";
 import { ZipReader } from "jsr:@zip-js/zip-js";
 
-interface EjfData {
-    characterChecksums: Map<string, number>;
+interface CharData {
+    checksum: number;
+    getData: NonNullable<Entry['getData']>;
 }
 
-async function readArchive(ejfPath: string): Promise<EjfData> {
+async function readArchive(ejfPath: string) {
     const archive = new ZipReader(await Deno.open(ejfPath));
     const entries = await archive.getEntries();
 
-    const characterChecksums = new Map<string, number>();
+    const chars = new Map<string, CharData>();
     for (const entry of entries) {
-        if (entry.filename.startsWith("0x")) {           
-            characterChecksums.set(entry.filename, entry.signature);
+        if (entry.filename.startsWith("0x")) {         
+            if (!entry.getData) {
+                throw new Error("Character with missing data function.");
+            }
+
+            chars.set(entry.filename, {
+                checksum: entry.signature,
+                getData: entry.getData
+            });
         }
     }
-
+    
     return {
-        characterChecksums
+        chars
     };
 }
 
-function findCharacterDifferences(firstEjfData: EjfData, secondEjfData: EjfData) {    
-    const firstCharacterSet = new Set<string>(firstEjfData.characterChecksums.keys());
-    const secondCharacterSet = new Set<string>(secondEjfData.characterChecksums.keys());
+type EjfData = Awaited<ReturnType<typeof readArchive>>;
+
+async function findCharacterDifferences(firstEjfData: EjfData, secondEjfData: EjfData) {    
+    const firstCharacterSet = new Set<string>(firstEjfData.chars.keys());
+    const secondCharacterSet = new Set<string>(secondEjfData.chars.keys());
 
     // Highlight removed characters.
     const removedCharacters = firstCharacterSet.difference(secondCharacterSet);
@@ -36,8 +49,41 @@ function findCharacterDifferences(firstEjfData: EjfData, secondEjfData: EjfData)
     for (const addedChar of addedCharacters) {
         console.log(`+${addedChar}`);
     }
+    
+    // Highlight changed characters.
+    const commonCharacters = firstCharacterSet.intersection(secondCharacterSet);
+    const changedCharacters = (Array.from(commonCharacters)).filter((ch) => firstEjfData.chars.get(ch) !== secondEjfData.chars.get(ch));
+    
+    for (const changedChar of changedCharacters) {
+        const firstCharData = firstEjfData.chars.get(changedChar);
+        const secondCharData = secondEjfData.chars.get(changedChar);
+
+        if (!firstCharData || !secondCharData) {
+            throw new Error("Unable to find character data for a character that has changed.");
+        }
+
+        await analyzeCharacterChange(firstCharData, secondCharData, changedChar);
+    }
+}
+
+async function analyzeCharacterChange(firstCharData: CharData, secondCharData: CharData, ch: string) {
+    const firstWriter = new BlobWriter("image/png");
+    const secondWriter = new BlobWriter("image/png");
+    
+    console.log(`Comparing ${ch}...`);
+    
+    await firstCharData.getData(firstWriter);
+    await secondCharData.getData(secondWriter);
+    
+    printImage({
+        rawFile: new Uint8Array(await (await firstWriter.getData()).arrayBuffer()),
+    });
+
+    printImage({
+        rawFile: new Uint8Array(await (await secondWriter.getData()).arrayBuffer())
+    });
 }
 
 const firstEjfData = await readArchive(firstFilePath);
 const secondEjfData = await readArchive(secondFilePath);
-findCharacterDifferences(firstEjfData, secondEjfData);
+await findCharacterDifferences(firstEjfData, secondEjfData);
