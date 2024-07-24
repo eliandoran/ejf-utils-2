26584b298ffb984eab978dd9b8bc2f6b7def8bf7
Elian Doran <contact@eliandoran.me>
2024-07-24 14:17:41 +0300

    Obtain per-character checksum

--- diff.ts
+++ diff.ts
@@ -2,14 +2,22 @@ const [ firstFilePath, secondFilePath ] = Deno.args;
 import { ZipReader } from "jsr:@zip-js/zip-js";
 
 async function readArchive(ejfPath: string) {
-    const archive = new ZipReader(await Deno.open(firstFilePath));
+    const archive = new ZipReader(await Deno.open(ejfPath));
     const entries = await archive.getEntries();
 
+    const characterChecksums = new Map<string, number>();
     for (const entry of entries) {
-        if (entry.filename.startsWith("0x")) {
-            console.log(entry.filename);
+        if (entry.filename.startsWith("0x")) {           
+            characterChecksums.set(entry.filename, entry.signature);
         }
     }
+
+    return {
+        characterChecksums
+    };
 }
 
-readArchive(firstFilePath);
+const firstEjfData = await readArchive(firstFilePath);
+const secondEjfData = await readArchive(secondFilePath);
+
+console.log(firstEjfData);
