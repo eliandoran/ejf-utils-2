7157880ddff8231bb2afc0c3ed8165351afa824d
Andreea Corlaci <andreea.corlaci@ausy.com>
2024-07-26 14:45:21 +0300

    Add po parsing

--- findMissingCharsInPo.ts
+++ findMissingCharsInPo.ts
@@ -1,9 +1,23 @@
+import getText from "npm:gettext-parser@8.0.0";
 const [ pathToPo ] = Deno.args;
 
+interface Message {
+    msgid: string;
+    comments: Record<string, unknown>;
+    msgstr: string[]
+}
+
 function processPoFile(path: string){
-    const poFile =  Deno.readTextFileSync(path).split("");
+    const poFile =  Deno.readTextFileSync(path);
     const charSet = new Set<string> (poFile);
-    console.log(charSet)
+    const po = getText.po.parse(poFile)
+        .translations[""] as Record<string, Message>;
+    delete po[""];
+    
+    const parsedPo = Object.values(po)
+        .map(el => el.msgstr[0]);
+
+    console.log(parsedPo)
 }
 
 processPoFile(pathToPo)
