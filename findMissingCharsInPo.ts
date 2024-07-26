ac53ec95ab865c827c4cde50911a991a65bb8606
Andreea Corlaci <andreea.corlaci@ausy.com>
2024-07-26 15:17:31 +0300

    Find the difference between po and ejf

--- findMissingCharsInPo.ts
+++ findMissingCharsInPo.ts
@@ -1,5 +1,10 @@
 import getText from "npm:gettext-parser@8.0.0";
-const [ pathToPo ] = Deno.args;
+import { ZipReader } from "jsr:@zip-js/zip-js";
+import { Entry } from "jsr:@zip-js/zip-js";
+
+const [ pathToPo, ejfFilePath] = Deno.args;
+console.log(pathToPo)
+console.log(ejfFilePath)
 
 interface Message {
     msgid: string;
@@ -16,9 +21,51 @@ function processPoFile(path: string){
     const parsedPo = new Set<string>( Object.values(po)
         .map(el => el.msgstr[0])
         .join("")
-        .split(""));
+        .split("")
+        .map(char => "0x" + char.charCodeAt(0).toString(16))
+    );
 
-    console.log(parsedPo)
+    return parsedPo;
 }
 
-processPoFile(pathToPo)
+interface CharData {
+    checksum: number;
+    getData: NonNullable<Entry['getData']>;
+}
+
+async function readEjfFile(ejfPath: string) {
+    const archive = new ZipReader(await Deno.open(ejfPath));
+    const entries = await archive.getEntries();
+
+    const chars = new Map<string, CharData>();
+    for (const entry of entries) {
+        if (entry.filename.startsWith("0x")) {         
+            if (!entry.getData) {
+                throw new Error("Character with missing data function.");
+            }
+
+            chars.set(entry.filename, {
+                checksum: entry.signature,
+                getData: entry.getData
+            });
+        }
+    }
+    
+    return {
+        chars
+    };
+}
+
+type EjfData = Awaited<ReturnType<typeof readEjfFile>>;
+
+async function findCharsDifferences() {
+    const poFile = processPoFile(pathToPo);
+    const ejfData = await readEjfFile(ejfFilePath);
+    const ejfFile = new Set<string>(ejfData.chars.keys());
+
+    const missingChars = poFile.difference(ejfFile);
+
+    console.log(missingChars)
+}
+
+findCharsDifferences();
