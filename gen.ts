e4c50135a6083dd376e00e480dfb2cb65a376a3d
Elian Doran <contact@eliandoran.me>
2024-07-29 17:15:58 +0300

    gen: Generate PNG of a single character

--- gen.ts
+++ gen.ts
@@ -1,3 +1,4 @@
+import { encode } from "https://deno.land/x/pngs/mod.ts";
 import freetype from "npm:freetype2";
 
 const ttfPath = Deno.args[0];
@@ -12,4 +13,27 @@ const glyph = face.loadChar("A".charCodeAt(0), {
     render: true
 });
 
-Deno.writeFileSync("glyph", glyph.bitmap?.buffer);
+function monochromeImageBufferToColorImageBuffer(inputBuffer: Uint8Array) {
+    const outputBuffer = new Uint8Array(4 * inputBuffer.length);
+    for (let i=0; i<inputBuffer.length; i++) {
+        outputBuffer[4 * i] = inputBuffer[i];
+        outputBuffer[4 * i + 1] = inputBuffer[i];
+        outputBuffer[4 * i + 2] = inputBuffer[i];
+        outputBuffer[4 * i + 3] = 255;
+    }
+
+    return outputBuffer;
+}
+
+function buildPng(colorImageBuffer: Uint8Array) {
+    const width = 23;
+    const numPixels = colorImageBuffer.length / 4;
+
+    console.log("Image size is ", colorImageBuffer.length);
+    return encode(colorImageBuffer, width, numPixels / width);    
+}
+
+const colorImageBuffer = monochromeImageBufferToColorImageBuffer(Uint8Array.from(glyph.bitmap?.buffer));
+const png = buildPng(colorImageBuffer);
+
+Deno.writeFileSync("glyph.png", png);
