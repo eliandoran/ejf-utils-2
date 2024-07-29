c81a16565eb3305f58f1fac59c6fb34238115bf1
Elian Doran <contact@eliandoran.me>
2024-07-29 22:03:45 +0300

    gen: Implement range parser

--- gen/main.ts
+++ gen/main.ts
@@ -1,6 +1,11 @@
+import buildEjf from "./ejf.ts";
 import Renderer from "./renderer.ts";
 
 const ttfPath = Deno.args[0];
 
-const renderer = new Renderer(ttfPath, 40);
-Deno.writeFileSync("glyph.png", renderer.render("A"));
+//const renderer = new Renderer(ttfPath, 40);
+//Deno.writeFileSync("glyph.png", renderer.render("A"));
+
+buildEjf({
+    charRange: "0x2d,0x30-0x3b,0x41-0x5b,0x61-0x7a"
+})
